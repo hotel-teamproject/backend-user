b9d9c097789f7fd04039748404793e6de68f6894
Kang-0318 <audiogarden2@kbu.ac.kr>
2025-11-19 21:03:51 +0900

    feat: add making3

--- models/AuditLog.js
+++ models/AuditLog.js
@@ -1,32 +1,12 @@
-const mongoose= require('mongoose')
-
+const mongoose = require("mongoose");
 const AuditLogSchema= new mongoose.Schema({
-    actor:{
-        type:mongoose.Types.ObjectId,
-        ref:'User'
-    },
-    role:String,
+  actor: { type: mongoose.Types.ObjectId, ref: "User" },
+  action: { type: String, enum: ["create", "login", "update", "delete", "reserve", "cancel"] },
+  resource: { type: String, enum: ["user", "reservation", "review"] },
+  targetId: { type: String },
+  diff: Object,
+  ip: String,
+  ua: String
+}, { timestamps: true });
 
-    resource:{
-        type:String,
-        enum:['post','user','file','report','setting']
-    },
-    action:{
-        type:String,
-        enum:['create','update','delete','approve','reject','hide']
-    },
-    targetId:{type:String},
-    diff:Object,
-    ip:String,
-    ua:String
-},{
-    timestamps:true
-})
-
-
-AuditLogSchema.index({createdAt:-1})
-
-AuditLogSchema.index({resource:1,action:1,createdAt:-1})
-
-
-module.exports=mongoose.model('AuditLog',AuditLogSchema)
+module.exports = mongoose.model("AuditLog", AuditLogSchema);
